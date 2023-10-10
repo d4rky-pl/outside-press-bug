3fdc3f4b80acf01a7d38dda11b8c4d0eac31e855
Michał Matyas <github@higher.lv>
2023-10-10 11:32:46 +0200

    Reproduce bug

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, SafeAreaView, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import OutsidePressHandler, { EventProvider } from "react-native-outside-press";
+import { useState, useEffect, createContext, useContext } from 'react'
+
+const SessionContext = createContext({});
+export const useSession = () => useContext(SessionContext);
+
+const Component = () => {
+  const { a } = useSession()
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <OutsidePressHandler onOutsidePress={() => null}>
+        <View />
+      </OutsidePressHandler>
+    </SafeAreaView>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <EventProvider>
+      <SessionContext.Provider value={{ a: () => null }}>
+        <Component />
+      </SessionContext.Provider>
+    </EventProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
